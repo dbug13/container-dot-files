c965f191b432bce5a09a410c4849cd4359c1a9dc
dbug13 <dbug13@gmail.com>
2024-11-04 07:22:43 -0800

    added mise installation script. Modified install.sh to only move .bashrc and .bash_profile if they exist

--- .local/scripts/install-mise.sh
+++ .local/scripts/install-mise.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 curl https://mise.run | sh
+
+echo "Added mise config to $HOME/.bashrc"
+echo "eval \"\$(/home/$USER/.local/bin/mise activate bash)\"" >>$HOME/.bashrc
+
+echo "remember to source the .bashrc file to activate up mise install"
--- install.sh
+++ install.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-ln -sf ./.bashrc ~/.bashrc
-ln -sf ./.bashrc ~/.bash_profile
-ln -sf ./.config/nvim ~/.config/nvim
+mkdir -p $HOME/.backups
+
+# Check if .bashrc exists and move if so
+if [ -f "$HOME/.bashrc" ]; then
+  mv "$HOME/.bashrc" "$HOME/.backups/"
+fi
+
+# Check if .bash_profile exists and move if so
+if [ -f "$HOME/.bash_profile" ]; then
+  mv "$HOME/.bash_profile" "$HOME/.backups/"
+fi
+
+ln -s $PWD/.bashrc ~/.bashrc
+ln -s $PWD/.bashrc ~/.bash_profile
+
+mkdir -p ~/.config
+ln -s $PWD/.config/nvim/ ~/.config/nvim
+
+mkdir -p ~/.local
+ln -s $PWD/.local/scripts/ ~/.local/scripts
