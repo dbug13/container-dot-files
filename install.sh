9a3787ace5aa35229c3f652d2e39f5167c4c8ba2
dbug13 <dbug13@gmail.com>
2024-11-04 18:38:08 -0800

    cleaned up the install.sh script

--- install.sh
+++ install.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-mkdir -p $HOME/.backups
+BACKUPS="$HOME/backups"
+mkdir -p $BACKUPS
 
-# Check if .bashrc exists and move if so
-if [ -f "$HOME/.bashrc" ]; then
-  mv "$HOME/.bashrc" "$HOME/.backups/"
-fi
+move_file() {
+  if [ -f "$1" ]; then
+    mv "$1" "$2"
+  fi
+}
 
-# Check if .bash_profile exists and move if so
-if [ -f "$HOME/.bash_profile" ]; then
-  mv "$HOME/.bash_profile" "$HOME/.backups/"
-fi
+sym_link() {
+  if [ -f "$1" ]; then
+    mkdir -p $(dirname "$2")
+    ln -s "$1" "$2"
+  fi
+}
 
-ln -s $PWD/.bashrc ~/.bashrc
-ln -s $PWD/.bashrc ~/.bash_profile
+# handle top level files
+move_file "$HOME/.bashrc" "${BACKUPS}/"
+move_file "$HOME/.bash_profile" "${BACKUPS}/"
 
+sym_link $PWD/.bashrc $HOME/.bashrc
+sym_link $PWD/.bashrc $HOME/.bash_profile
+
+# handle .config files
 mkdir -p ~/.config
-ln -s $PWD/.config/nvim/ ~/.config/nvim
+mkdir -p "${BACKUPS}/.config"
 
+move_file $HOME/.config/nvim "${BACKUPS}/.config/"
+sym_link $PWD/.config/nvim $HOME/.config/nvim
+
+# handle .local files
 mkdir -p ~/.local
-ln -s $PWD/.local/scripts/ ~/.local/scripts
+mkdir -p "${BACKUPS}/.local"
+
+sym_link $PWD/.local/scripts $HOME/.local/scripts
